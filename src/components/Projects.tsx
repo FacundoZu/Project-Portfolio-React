ed224b9f32c4476a77a5a598834500d8552818e8
Facundo Zuleta <facundozuleta70@gmail.com>
2025-11-06 14:06:10 -0300

    fix mb, project's section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -22,7 +22,7 @@ export default function Projects() {
       className="w-full text-white py-16"
     >
       <motion.div
-        className="text-center"
+        className="text-center mb-8"
         initial={{ opacity: 0, y: -30 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 1 }}
